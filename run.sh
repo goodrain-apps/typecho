b6af8be444237634b4d1a8eee0ce24f8e01ad645
周悦秋 <ethan@goodrain.com>
2016-01-03 21:16:29 +0800

    添加run.sh 和Procfile 文件

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# 判断并选择是否初始化数据库
+[ ! -e /data/.dbimported ] && php importdb.php && touch /data/.dbimported
+
+
+# 将需要持久化的目录移动到 /data 目录
+if [ ! -d /data/usr ];then
+  mv /app/usr /data/
+  ln -s /data/usr /app/
+elif [ -d /app/usr ];then
+  rm -rf /app/usr
+  ln -s /data/user /app/
+fi
+
+# 启动web server
+vendor/bin/heroku-php-apache2
